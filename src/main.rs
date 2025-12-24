348e31bab0bac4ce833ca69991cde05c53d1a115
ssuverkropp <sander.suverkropp@proton.me>
2025-12-24 20:08:09 +0100

    Solve 2025 day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input/2025/day1.txt").expect("No input file");
+
+    let mut zeros = 0;
+    let mut dial = 50;
+    for line in contents.lines() {
+        let first_letter = line.chars().nth(0);
+        let sign = match first_letter {
+            Some('L') => -1,
+            Some('R') => 1,
+            _ => panic!("Line doesn't start with L or R"),
+        };
+        let distance = line[1..].parse::<i32>().unwrap();
+        dial = dial + sign * distance;
+        dial = dial % 100;
+        if dial == 0 {
+            zeros += 1
+        }
+    }
+    println!("{zeros}");
 }
