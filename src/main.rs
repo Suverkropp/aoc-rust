a6d86fb7fd6f5f040eb995854af67099a97d6766
ssuverkropp <sander.suverkropp@proton.me>
2025-12-25 01:18:52 +0100

    Solve 2025 day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,54 @@
-use std::fs;
+use std::{fs, str::Lines};
 
 fn main() {
-    let contents = fs::read_to_string("input/2025/day1.txt").expect("No input file");
+    let res = part2(get_input().lines());
+    println!("The result is {res}.")
+}
 
+fn get_input() -> String {
+    fs::read_to_string("input/2025/day1.txt").expect("No input file")
+}
+
+fn parse_line(line: &str) -> i32 {
+    let first_letter = line.chars().nth(0);
+    let sign = match first_letter {
+        Some('L') => -1,
+        Some('R') => 1,
+        _ => panic!("Line doesn't start with L or R"),
+    };
+    let distance = line[1..].parse::<i32>().unwrap();
+    sign * distance
+}
+
+fn part1(lines: Lines) -> i32 {
     let mut zeros = 0;
     let mut dial = 50;
-    for line in contents.lines() {
-        let first_letter = line.chars().nth(0);
-        let sign = match first_letter {
-            Some('L') => -1,
-            Some('R') => 1,
-            _ => panic!("Line doesn't start with L or R"),
-        };
-        let distance = line[1..].parse::<i32>().unwrap();
-        dial = dial + sign * distance;
-        dial = dial % 100;
+    for line in lines {
+        let num = parse_line(line);
+        dial = (dial + num) % 100;
         if dial == 0 {
             zeros += 1
         }
     }
-    println!("{zeros}");
+    zeros
 }
+
+fn part2(lines: Lines) -> i32 {
+    let mut zeros = 0;
+    let mut dial = 50;
+    for line in lines {
+        let num = parse_line(line);
+        let old = dial;
+        dial = dial + num;
+        if dial == 0 {
+            zeros += 1
+        }
+        if old.signum() == -dial.signum() {
+            zeros += 1;
+        }
+        zeros += (dial / 100).abs();
+        dial = dial % 100;
+    }
+    zeros
+}
+
