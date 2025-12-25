719ae4b2b161081099a9b7df7e651e90061d4b6e
ssuverkropp <sander.suverkropp@proton.me>
2025-12-25 11:36:48 +0100

    Solve 2025 day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-mod year2025;
 mod aoc;
+mod year2025;
 
 use year2025::day2;
 
 fn main() {
     let input = aoc::get_input(2025, 2);
     let input = day2::handle_input(&input);
-    let res = day2::part1(input);
+    let res = day2::part2(input);
     println!("The result is {res}.")
 }
-
--- src/year2025/day2.rs
+++ src/year2025/day2.rs
@@ -1,15 +1,15 @@
 pub fn handle_input(input: &String) -> impl Iterator<Item = (u64, u64)> {
-    input.split(',')
-        .map(|range| {
+    input.split(',').map(|range| {
         let (begin, end) = range.split_once('-').unwrap();
-        let begin:u64 = begin.parse::<u64>().unwrap();
-        let end :u64 = end.parse::<u64>().unwrap();
+        let begin: u64 = begin.parse::<u64>().unwrap();
+        let end: u64 = end.parse::<u64>().unwrap();
         (begin, end)
     })
 }
 
-pub fn part1(ranges : impl Iterator<Item=(u64, u64)>) -> u64 {
-    ranges.flat_map(|(begin, end)| { begin..end+1 })
+pub fn part1(ranges: impl Iterator<Item = (u64, u64)>) -> u64 {
+    ranges
+        .flat_map(|(begin, end)| begin..end + 1)
         .filter(|id| is_double(*id))
         .sum()
 }
@@ -17,8 +17,27 @@ pub fn part1(ranges : impl Iterator<Item=(u64, u64)>) -> u64 {
 fn is_double(id: u64) -> bool {
     let id = id.to_string();
     if id.len() % 2 != 0 {
-        return false
+        return false;
     }
     let (begin, end) = id.split_at(id.len() / 2);
     begin == end
+}
+
+pub fn part2(ranges: impl Iterator<Item = (u64, u64)>) -> u64 {
+    ranges
+        .flat_map(|(begin, end)| begin..end + 1)
+        .filter(|id| is_repeated(*id))
+        .sum()
+}
+
+fn is_repeated(id: u64) -> bool {
+    let id = id.to_string();
+    let len = id.len();
+    for i in 1..len/2+1 {
+        let seg = id.split_at(i).0;
+        if id == seg.repeat(len/i) {
+            return true;
+        }
+    }
+    false
 }
