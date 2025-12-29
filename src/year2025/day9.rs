caf0faa1caaa65a831b871ce722c6fac84f49f42
ssuverkropp <sander.suverkropp@proton.me>
2025-12-29 15:07:52 +0100

    Improve 2025 day 9 efficiency by using queue, not stack

--- src/year2025/day9.rs
+++ src/year2025/day9.rs
@@ -1,6 +1,7 @@
 use crate::grid::Grid;
-use itertools::{Itertools, iproduct};
-use std::cmp::{max, min};
+use itertools::{iproduct, Itertools};
+use std::cmp::{max, min, Reverse};
+use std::collections::VecDeque;
 
 pub type Position = (usize, usize);
 
@@ -14,31 +15,36 @@ fn read_position(line: &str) -> Position {
 }
 
 pub fn part1(red_tiles: &Vec<Position>) -> usize {
-    red_tiles
-        .iter()
-        .tuple_combinations::<(_, _)>()
-        .map(|(p1, p2)| square_size(p1, p2))
-        .max()
-        .unwrap()
+    let (_corners, area) = rectangles_by_area(red_tiles)[0];
+    area
 }
 
-fn square_size((x1, y1): &Position, (x2, y2): &Position) -> usize {
+fn rectangles_by_area(red_tiles: &Vec<Position>) -> Vec<((Position, Position), usize)> {
+    let mut pairs: Vec<_> = red_tiles
+        .iter()
+        .tuple_combinations::<(_, _)>()
+        .map(|(p1, p2)| ((*p1, *p2), rectangle_area(p1, p2)))
+        .collect();
+    pairs.sort_by_key(|(_, area)| Reverse(*area));
+    pairs
+}
+
+fn rectangle_area((x1, y1): &Position, (x2, y2): &Position) -> usize {
     let x_dist = x1.abs_diff(*x2) + 1;
     let y_dist = y1.abs_diff(*y2) + 1;
     x_dist * y_dist
 }
 
 pub fn part2(red_tiles: &Vec<Position>) -> usize {
-    let pairs = red_tiles.iter().tuple_combinations::<(_, _)>();
-
     let green_tiles = find_green_tiles(red_tiles);
-    green_tiles.print_by(|b| if *b {'X'} else {'.'});
 
-    pairs
-        .filter(|(p1, p2)| in_square_green(&green_tiles, **p1, **p2))
-        .map(|(p1, p2)| square_size(p1, p2))
-        .max()
-        .unwrap()
+    let rectangles = rectangles_by_area(red_tiles);
+    rectangles
+        .iter()
+        .filter(|((p1, p2), _area)| in_rectangle_green(&green_tiles, *p1, *p2))
+        .map(|((_, _), area)| *area)
+        .next()
+        .expect("No rectangles completely green")
 }
 
 fn find_green_tiles(red_tiles: &Vec<Position>) -> Grid<bool> {
@@ -54,11 +60,17 @@ fn in_lines(lines: &Grid<bool>) -> Grid<bool> {
     let height = lines.get_height();
     let mut in_lines = Grid::new(width, height, true);
     let mut visited = Grid::new(width, height, false);
-    let mut queue = Vec::new();
-    queue.push((0, 0));
+    let mut queue = VecDeque::new();
+    queue.push_back((0, 0));
     visited.set(0, 0, true);
+    let mut counter = 0;
 
-    while let Some((x, y)) = queue.pop() {
+    while let Some((x, y)) = queue.pop_front() {
+        counter += 1;
+        if counter % 10_000_000 == 0 {
+            println!("Counter: {}", counter);
+            println!("Queue size: {}", queue.len());
+        }
         if lines.get(x, y).unwrap() {
             continue;
         }
@@ -76,13 +88,14 @@ fn in_lines(lines: &Grid<bool>) -> Grid<bool> {
             add_to_queue(&mut queue, &mut visited, x, y + 1);
         }
     }
+    println!("Tiles coloured green (or red): {counter}/{}", width*height);
     in_lines
 }
 
-fn add_to_queue(queue: &mut Vec<(usize, usize)>, visited: &mut Grid<bool>, x: usize, y: usize) {
+fn add_to_queue(queue: &mut VecDeque<(usize, usize)>, visited: &mut Grid<bool>, x: usize, y: usize) {
     if !visited.get(x, y).unwrap() {
         visited.set(x, y, true);
-        queue.push((x, y));
+        queue.push_back((x, y));
     }
 }
 
@@ -118,15 +131,13 @@ fn mark_line(grid: &mut Grid<bool>, (x1, y1): &Position, (x2, y2): &Position) {
     }
 }
 
-fn in_square_green(green_tiles: &Grid<bool>, (x1, y1): Position, (x2, y2): Position) -> bool {
+fn in_rectangle_green(green_tiles: &Grid<bool>, (x1, y1): Position, (x2, y2): Position) -> bool {
     let x_min = min(x1, x2);
     let x_max = max(x1, x2);
     let y_min = min(y1, y2);
     let y_max = max(y1, y2);
 
-    iproduct!(x_min..=x_max, y_min..=y_max).all(|(x, y)| {
-        green_tiles.get(x, y).unwrap()
-    })
+    iproduct!(x_min..=x_max, y_min..=y_max).all(|(x, y)| green_tiles.get(x, y).unwrap())
 }
 
 #[cfg(test)]
